b01ffd09437ea1567a0bc7a6eafce8dd883e5880
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-04-27 12:34:54 +0200

    upd

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
 public class Inventory {
-    private
+
+    private ArrayList<Item> items; // DECLARING arraylist
+
+    public Inventory(){
+        items = new ArrayList<>(); // ASSIGNING an arraylist
+    }
+
+    public void addItem(Item item) { // Item != item
+        items.add(item);
+    }
+
+    public void displayInventory(){
+        for (Item item : items ){
+            System.out.println("Item: " + item.getName() + ", Quantity: " +item.getQuantity());
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
-    /*
+
     public static void main(String[] args)  {
-        System.out.println("hello world");
+
+        Inventory inventory = new Inventory();
+
+        Item item1 = new Item(name: "Apples", quantity: 20);
+        Item item2 = new Item(name: "Sword ", quantity: 2);
+
+
+        inventory.addItem();
     }
-    */
+
 
 }
